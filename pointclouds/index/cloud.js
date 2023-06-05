f55d784c25479eb0aa97a3a89528fcb8d0665d0c
ivercruz <cruziv2590@gmail.com>
2023-06-05 14:39:34 -0300

    Initial commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=tmerc +lat_0=-90 +lon_0=-66 +k=1 +x_0=3500000 +y_0=0 +datum=NAD83 +units=m +no_defs",
+    "points": 90249920,
+    "boundingBox": {
+        "lx": 3563867.85,
+        "ly": 7308603.796,
+        "lz": 1487.808,
+        "ux": 3564970.734,
+        "uy": 7309706.68,
+        "uz": 2590.692000000078
+    },
+    "tightBoundingBox": {
+        "lx": 3563867.85,
+        "ly": 7308603.796,
+        "lz": 1487.808,
+        "ux": 3564970.734,
+        "uy": 7309617.937,
+        "uz": 1602.192
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 9.551255226135254,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
